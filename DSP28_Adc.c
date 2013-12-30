0c87841cb6ac6cbd0b114364b95c7ec6987d91a8
magicfillcs <magicfillcs@gmail.com>
2013-12-30 13:35:49 +0800

    Update DSP28_Adc.c

--- DSP28_Adc.c
+++ DSP28_Adc.c
@@ -1,4 +1,3 @@
-
 /*************************************************
 Copyright (C), 1988-2013, MagicFill Tech. Co., Ltd.
 
@@ -54,10 +53,98 @@ History: // 修改历史记录列表，每条修改记录应包括修改日期�
 *
 ****************************************************************************/
 
-void InitAdc(void)
-{
-    
+    unsigned int i;
+	
+	AdcRegs.ADCTRL1.bit.RESET=1;		//[User]复位
+	NOP;
+	AdcRegs.ADCTRL1.bit.RESET=0;
+	
+	AdcRegs.ADCTRL1.bit.SUSMOD=3;	//仿真暂停时，序列发生器和其他数字电路逻辑立即停止
+	
+	AdcRegs.ADCTRL1.bit.ACQ_PS=0;	//采样窗口大小，SOC脉冲宽度为1个ADCLK 
+/****************************************
+[User][Date:2013.12.26]
+
+SOC脉冲宽度1等于 ACQ_PS的值 + 1
+****************************************/
+
+	
+	AdcRegs.ADCTRL1.bit.CPS=0;      	//核时钟预定标器，等于0，未将时钟进行2分频
+	
+	AdcRegs.ADCTRL1.bit.CONT_RUN=0;   	//运行于启动/停止模式
+
+/****************************************
+[User][Date:2013.12.26]
+
+级联方式，SEQ1和SEQ2级联起来，作为一个16状态序列发生器SEQ
+****************************************/	
+    AdcRegs.ADCTRL1.bit.SEQ_CASC=1;    //级联方式
+
+
+
+/****************************************
+[User][Date:2013.12.26]
+
+参考ADCTRL3寄存器中对各bit的定义
+****************************************/	
+	AdcRegs.ADCTRL3.bit.ADCBGRFDN=3;
+	for(i=0;i<10000;i++)	NOP;
+	AdcRegs.ADCTRL3.bit.ADCPWDN=1;
+	for(i=0;i<5000;i++)	NOP;
+	
+
+
+	AdcRegs.ADCTRL3.bit.ADCCLKPS=15;     //ADCLK=HSPCLK/30
+	
+	AdcRegs.ADCTRL3.bit.SMODE_SEL=0;     //采用顺序采样模式
+	
+	AdcRegs.MAX_CONV.bit.MAX_CONV=15;    //总共采样16路
+	//////[User][###不懂###] 
+
+
+	AdcRegs.CHSELSEQ1.bit.CONV00=0;
+	AdcRegs.CHSELSEQ1.bit.CONV01=1;
+	AdcRegs.CHSELSEQ1.bit.CONV02=2;
+	AdcRegs.CHSELSEQ1.bit.CONV03=3;
+	
+	AdcRegs.CHSELSEQ2.bit.CONV04=4;
+	AdcRegs.CHSELSEQ2.bit.CONV05=5;
+	AdcRegs.CHSELSEQ2.bit.CONV06=6;
+	AdcRegs.CHSELSEQ2.bit.CONV07=7;
+	
+	AdcRegs.CHSELSEQ3.bit.CONV08=8;
+	AdcRegs.CHSELSEQ3.bit.CONV09=9;
+	AdcRegs.CHSELSEQ3.bit.CONV10=10;
+	AdcRegs.CHSELSEQ3.bit.CONV11=11;
+	
+	AdcRegs.CHSELSEQ4.bit.CONV12=12;
+	AdcRegs.CHSELSEQ4.bit.CONV13=13;
+	AdcRegs.CHSELSEQ4.bit.CONV14=14;
+	AdcRegs.CHSELSEQ4.bit.CONV15=15;
+	
+	AdcRegs.ADC_ST_FLAG.bit.INT_SEQ1_CLR=1;
+	//清除SEQ1中的中断标志位INT_SEQ1
+	
+	AdcRegs.ADC_ST_FLAG.bit.INT_SEQ2_CLR=1;	
+	//清除SEQ2中的中断标志位INT_SEQ2
+	
+	AdcRegs.ADCTRL2.bit.EVB_SOC_SEQ=0;
+	AdcRegs.ADCTRL2.bit.RST_SEQ1=0;
+	AdcRegs.ADCTRL2.bit.INT_ENA_SEQ1=1;
+	AdcRegs.ADCTRL2.bit.INT_MOD_SEQ1=0;
+	AdcRegs.ADCTRL2.bit.EVA_SOC_SEQ1=0;
+	AdcRegs.ADCTRL2.bit.EXT_SOC_SEQ1=0;
+	AdcRegs.ADCTRL2.bit.RST_SEQ2=0;
+	AdcRegs.ADCTRL2.bit.SOC_SEQ2=0;
+	AdcRegs.ADCTRL2.bit.INT_ENA_SEQ2=0;
+	AdcRegs.ADCTRL2.bit.INT_MOD_SEQ2=0;
+	AdcRegs.ADCTRL2.bit.EVB_SOC_SEQ2=0;
+	AdcRegs.ADCTRL2.bit.SOC_SEQ1=1;
+	
+	
 }	
+
+
 
 
 //===========================================================================
